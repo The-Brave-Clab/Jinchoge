914adde7ed4d1922eff95f70d662b29bc12ce98f
Runxuan Shu <runxuan.shu@unity3d.com>
2022-01-10 12:01:02 +0800

    process symmetric friend request

--- Yuyuyui.PrivateServer/Entity/UserData/Friends/SendFellowRequestEntity.cs
+++ Yuyuyui.PrivateServer/Entity/UserData/Friends/SendFellowRequestEntity.cs
@@ -18,26 +18,43 @@ namespace Yuyuyui.PrivateServer
         {
             var player = GetPlayerFromCookies();
             string friendCode = GetPathParameter("user_id");
-            
+
             // Get the requested player
             // Respects the path parameter
             var friend = PlayerProfile.Load(friendCode);
-            
+
+            FriendRequest friendRequest;
+
             // The game client checks if the friend has already been added
             // so we don't check it here.
-            
-            // We don't care about the request body anymore
-            // {
-            //     "user_id" = "1234567890"
-            // }
 
-            var friendRequest = FriendRequest.CreateOrLoad(player, friend);
+            // However, we check if the same request have sent from the other player
+            // if so, we should automatically make it accepted
+            try
+            {
+                friendRequest =
+                    player.friendRequests
+                        .Select(FriendRequest.Load)
+                        .First(fr => fr.fromUser == friend.id.code);
+                Utils.Log($"Found symmetric friend request {friendRequest.id} ({friendRequest.fromUser}=>{friendRequest.toUser})!");
+                friendRequest.status = 1; // Accept
+                friendRequest.ProcessStatus();
+            }
+            catch (InvalidOperationException)
+            {
+                // We don't care about the request body anymore
+                // {
+                //     "user_id" = "1234567890"
+                // }
+
+                friendRequest = FriendRequest.CreateOrLoad(player, friend);
+            }
 
             Response responseObj = new()
             {
                 fellow_request = friendRequest
             };
-            
+
             responseBody = Serialize(responseObj);
             SetBasicResponseHeaders();
 
