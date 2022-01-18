869ca4526db4d42b69bb70da0e3dd3be47d2e1ef
Runxuan Shu <runxuan.shu@unity3d.com>
2022-01-18 14:15:09 +0800

    removed unused code

--- Yuyuyui.PrivateServer.DataModel/BaseContext.cs
+++ Yuyuyui.PrivateServer.DataModel/BaseContext.cs
@@ -29,16 +29,4 @@ namespace Yuyuyui.PrivateServer.DataModel
             }
         }
     }
-
-    public static class EFCoreExtensions
-    {
-        public static IAsyncEnumerable<TEntity> AsAsyncEnumerable<TEntity>(this Microsoft.EntityFrameworkCore.DbSet<TEntity> obj) where TEntity : class
-        {
-            return Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions.AsAsyncEnumerable(obj);
-        }
-        public static IQueryable<TEntity> Where<TEntity>(this Microsoft.EntityFrameworkCore.DbSet<TEntity> obj, System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate) where TEntity : class
-        {
-            return System.Linq.Queryable.Where(obj, predicate);
-        }
-    }
 }
