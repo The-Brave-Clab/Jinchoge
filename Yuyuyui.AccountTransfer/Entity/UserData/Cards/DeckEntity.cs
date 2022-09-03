e7a344338a1df2d64f3c4a19eb66929f30899009
Runxuan Shu <shurunxuan@hotmail.com>
2022-09-03 22:22:32 +0800

    fixed deck duplication

--- Yuyuyui.AccountTransfer/Entity/UserData/Cards/DeckEntity.cs
+++ Yuyuyui.AccountTransfer/Entity/UserData/Cards/DeckEntity.cs
@@ -23,33 +23,34 @@ namespace Yuyuyui.AccountTransfer
         {
             var response = Deserialize<PrivateServer.DeckEntity.Response>(responseBody)!;
 
-            foreach (var d in response.decks)
+            playerSession.player!.decks = response.decks.Select(d =>
             {
-                Deck deck = new Deck()
+                Deck deck = new Deck
                 {
                     id = d.id,
                     leaderUnitID = d.leader_deck_card_id,
                     name = d.name,
-                    units = new List<long>()
-                };
-                
-                foreach (var u in d.cards)
-                {
-                    Unit unit = new Unit()
+                    units = d.cards.Select(u =>
                     {
-                        id = u.id,
-                        baseCardID = u.user_card_id,
-                        supportCardID = u.support.ContainsKey("user_card_id") ? u.support["user_card_id"] : null,
-                        supportCard2ID = u.support_2.ContainsKey("user_card_id") ? u.support_2["user_card_id"] : null,
-                        assistCardID = u.assist.ContainsKey("user_card_id") ? u.assist["user_card_id"] : null,
-                        accessories = u.accessories.Select(a => a.id).ToList()
-                    };
-                    deck.units.Add(unit.id);
-                    unit.Save();
-                }
-                playerSession.player!.decks.Add(deck.id);
+                        Unit unit = new Unit
+                        {
+                            id = u.id,
+                            baseCardID = u.user_card_id,
+                            supportCardID = u.support.ContainsKey("user_card_id") ? u.support["user_card_id"] : null,
+                            supportCard2ID = u.support_2.ContainsKey("user_card_id") ? u.support_2["user_card_id"] : null,
+                            assistCardID = u.assist.ContainsKey("user_card_id") ? u.assist["user_card_id"] : null,
+                            accessories = u.accessories.Select(a => a.id).ToList()
+                        };
+                        unit.Save();
+                        return u.id;
+                    }).ToList()
+                };
+
                 deck.Save();
-            }
+
+                return d.id;
+            }).ToList();
+
             playerSession.player!.Save();
             
             Utils.LogTrace($"Got decks.");
