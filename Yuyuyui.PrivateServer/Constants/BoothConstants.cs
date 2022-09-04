03b6e299e3f9f461b996f49cb3797601e033b358
pkchan1013 <pkchan110a@hotmail.com>
2022-09-04 21:21:52 +0800

    feat: Update Taisha Point Exchange List to include all collab characters

--- Yuyuyui.PrivateServer/Constants/BoothConstants.cs
+++ Yuyuyui.PrivateServer/Constants/BoothConstants.cs
@@ -5,92 +5,102 @@ namespace Yuyuyui.PrivateServer.Constants;
 
 public static class BoothConstants
 {
-    public static BoothResponse ExchangeItemResponse = new BoothResponse(
-        exchange: new BoothExchange(
-            id: 1,
-            startAt: 1483228800,
-            endAt: 1893456000,
-            products: new Dictionary<string, BoothExchangeProduct>()
-            {
+    public static BoothResponse InitExchangeItemResponse()
+    {
+        return new BoothResponse(
+            exchange: new BoothExchange(
+                exchangeId: 100000000,
+                startAt: 1483228800,
+                endAt: 1893456000,
+                products: new Dictionary<string, BoothExchangeProduct>()
                 {
-                    "1", 
-                    new BoothExchangeProduct(
-                        id: 1,
-                        masterId: 500010,
-                        fromContentId: 21,
-                        itemCategory: 1,
-                        point: 10000,
-                        name: "源モモ",
-                        quantity: 1
-                    )
-                },
-                {
-                    "2", 
-                    new BoothExchangeProduct(
-                        id: 2,
-                        masterId: 500020,
-                        fromContentId: 21,
-                        itemCategory: 1,
-                        point: 10000,
-                        name: "半蔵門雪",
-                        quantity: 1
-                    )
-                },
-                {
-                    "3", 
-                    new BoothExchangeProduct(
-                        id: 3,
-                        masterId: 500030,
-                        fromContentId: 21,
-                        itemCategory: 1,
-                        point: 10000,
-                        name: "八千代命",
-                        quantity: 1
-                    )
-                },
-                {
-                    "4", 
-                    new BoothExchangeProduct(
-                        id: 4,
-                        masterId: 500040,
-                        fromContentId: 21,
-                        itemCategory: 1,
-                        point: 10000,
-                        name: "相模楓",
-                        quantity: 1
-                    )
-                },
-                {
-                    "5", 
-                    new BoothExchangeProduct(
-                        id: 5,
-                        masterId: 500050,
-                        fromContentId: 21,
-                        itemCategory: 1,
-                        point: 10000,
-                        name: "青葉初芽",
-                        quantity: 1
-                    )
-                },
-                {
-                    "6", 
-                    new BoothExchangeProduct(
-                        id: 6,
-                        masterId: 500060,
-                        fromContentId: 21,
-                        itemCategory: 1,
-                        point: 10000,
-                        name: "石川五恵",
-                        quantity: 1
-                    )
+                    {
+                        "1", NewCardExchangeProduct(id: 100000001, masterId: 500140, name: "結城友奈")
+                    },
+                    {
+                        "2", NewCardExchangeProduct(id: 100000002, masterId: 500150, name: "東郷美森")
+                    },
+                    {
+                        "3", NewCardExchangeProduct(id: 100000003, masterId: 500160, name: "犬吠埼風")
+                    },
+                    {
+                        "4", NewCardExchangeProduct(id: 100000004, masterId: 500170, name: "犬吠埼樹")
+                    },
+                    {
+                        "5", NewCardExchangeProduct(id: 100000005, masterId: 500180, name: "犬吠埼樹")
+                    },
+                    {
+                        "6", NewCardExchangeProduct(id: 100000006, masterId: 500190, name: "乃木園子")
+                    },
+                    {
+                        "7", NewCardExchangeProduct(id: 100000007, masterId: 500010, name: "源モモ")
+                    },
+                    {
+                        "8", NewCardExchangeProduct(id: 100000008, masterId: 500020, name: "半蔵門雪")
+                    },
+                    {
+                        "9", NewCardExchangeProduct(id: 100000009, masterId: 500030, name: "八千代命")
+                    },
+                    {
+                        "10", NewCardExchangeProduct(id: 100000010, masterId: 500040, name: "相模楓")
+                    },
+                    {
+                        "11", NewCardExchangeProduct(id: 100000011, masterId: 500050, name: "青葉初芽")
+                    },
+                    {
+                        "12", NewCardExchangeProduct(id: 100000012, masterId: 500060, name: "石川五恵")
+                    },
+                    {
+                        "13", NewCardExchangeProduct(id: 100000013, masterId: 500070, name: "衛藤可奈美")
+                    },
+                    {
+                        "14", NewCardExchangeProduct(id: 100000014, masterId: 500080, name: "十条姫和")
+                    },
+                    {
+                        "15", NewCardExchangeProduct(id: 100000015, masterId: 500090, name: "獅童真希")
+                    },
+                    {
+                        "16", NewCardExchangeProduct(id: 100000016, masterId: 520000, name: "衛藤可奈美")
+                    },
+                    {
+                        "17", NewCardExchangeProduct(id: 100000017, masterId: 530000, name: "御坂美琴")
+                    },
+                    {
+                        "18", NewCardExchangeProduct(id: 100000017, masterId: 530010, name: "白井黒子")
+                    },
+                    {
+                        "19", NewCardExchangeProduct(id: 100000019, masterId: 500100, name: "初春飾利")
+                    },
+                    {
+                        "20", NewCardExchangeProduct(id: 100000020, masterId: 500110, name: "佐天涙子")
+                    },
+                    {
+                        "21", NewCardExchangeProduct(id: 100000021, masterId: 500120, name: "御坂美琴")
+                    },
+                    {
+                        "22", NewCardExchangeProduct(id: 100000022, masterId: 500130, name: "白井黒子")
+                    }
                 }
-            }
-        )
-    );
+            )
+        );
+    }
     
     public static BoothResponse TradeItemResponse = new BoothResponse(
         exchange: new BoothExchange()
     );
     
     public static BoothEventResponse EventItemResponse = new BoothEventResponse();
+
+    private static BoothExchangeProduct NewCardExchangeProduct(long id, long masterId, string name)
+    {
+        return new BoothExchangeProduct(
+            id: id,
+            masterId: masterId,
+            fromContentId: 21,
+            itemCategory: 1,
+            point: 10000,
+            name: name,
+            quantity: 1
+        );
+    }
 }
