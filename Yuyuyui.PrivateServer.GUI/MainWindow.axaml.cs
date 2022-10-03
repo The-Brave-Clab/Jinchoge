3836f995a88106958d7bbe57a831ea35a5ec003f
Runxuan Shu <shurunxuan@hotmail.com>
2022-10-04 02:42:16 +0800

    added basic avalonia ui project

---
+++ Yuyuyui.PrivateServer.GUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Yuyuyui.PrivateServer.GUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
