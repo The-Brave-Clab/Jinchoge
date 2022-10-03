8e867103ce4e72a6481e7db413a297b5759fc497
Runxuan Shu <shurunxuan@hotmail.com>
2022-10-03 18:59:11 +0800

    eliminated build warnings

--- Yuyuyui.PrivateServer/Entity/RequestErrorEntity.cs
+++ Yuyuyui.PrivateServer/Entity/RequestErrorEntity.cs
@@ -51,7 +51,7 @@ namespace Yuyuyui.PrivateServer
             // A0120: Input Error
             // A4101: HTTP 422, Login Failed, ログインに失敗しました。 このメッセージが繰り返し表示される場合、 お問い合わせフォームよりご連絡ください。
             // U0401: HTTP 401, Unauthorized Error
-            string messageBody = string.IsNullOrEmpty(logMessage) ? responseObj.error.message : logMessage;
+            string messageBody = string.IsNullOrEmpty(logMessage) ? responseObj.error.message : logMessage!;
             
             Utils.LogError($"<APIError> {responseObj.error.code}: {messageBody}");
             
