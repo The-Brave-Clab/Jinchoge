dd68c7a7630758c852f757429e398f7d644d14e9
Runxuan Shu <shurunxuan@hotmail.com>
2022-10-08 20:09:12 +0800

    convert help subviews to mvvm

---
+++ Yuyuyui.PrivateServer.GUI/ViewModels/HelpSubViewModels/IntroductionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using Yuyuyui.PrivateServer.GUI.Views;
+
+namespace Yuyuyui.PrivateServer.GUI.ViewModels;
+
+public class IntroductionViewModel : HelpSubViewModelBase
+{
+    public IntroductionViewModel()
+    {
+        PrivateServerButtonText = HelpViewModel.ButtonInfo[typeof(PrivateServerView)];
+    }
+    
+    private string privateServerButtonText = "";
+
+    public string PrivateServerButtonText
+    {
+        get => privateServerButtonText;
+        set => this.RaiseAndSetIfChanged(ref privateServerButtonText, value);
+    }
+
+    public void PrivateServerButtonCommand()
+    {
+        HelpViewModel!.PushPage(typeof(PrivateServerView));
+    }
+}
