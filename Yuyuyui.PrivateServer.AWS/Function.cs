01b70dd25b10963642d8f2f4af480847ec2c72bc
Runxuan Shu <shurunxuan@hotmail.com>
2025-10-08 12:45:42 +0800

    Adds initial AWS Lambda project scaffold
    Introduces a basic AWS Lambda .NET project with an empty handler and deployment configuration.
    Prepares the solution for serverless integration and future endpoint implementation.

---
+++ Yuyuyui.PrivateServer.AWS/Function.cs
@@ -0,0 +1,17 @@
+using Amazon.Lambda.Core;
+
+namespace Yuyuyui.PrivateServer.AWS;
+
+public class Function
+{
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <returns></returns>
+    public string FunctionHandler(string input, ILambdaContext context)
+    {
+        return input.ToUpper();
+    }
+}
